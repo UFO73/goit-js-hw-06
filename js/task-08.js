b8608c87d486fc4cf81262986f0494045a4f4d8c
UFO73 <118683335+UFO73@users.noreply.github.com>
2023-03-30 01:36:09 +0300

    10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onSubmit)
+ 
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const elementIntut =  evt.currentTarget.elements;
+    
+    if (elementIntut[0].value.length === 0 || elementIntut[1].value.length === 0){
+        window.alert("Поля повинні бути заповнені!");
+    }else{
+        const { email, password } = evt.currentTarget.elements;
+        const data = {
+            email: email.value,
+            password: password.value,
+        };
+        
+        console.log(data);
+    }
+}
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const text = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener('click', onClick);
+
+function onClick(){
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+}
